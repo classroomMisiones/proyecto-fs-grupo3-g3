ae5f1aff0a59d0c90ccb58f9602498a2159a33ad
marceloedelgado <marceloedelgado@gmail.com>
2021-07-21 14:09:25 -0300

    commit con home navbar footer estilos y arreglos de bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './home/login/login.component';
+import { CentralComponent } from './home/central/central.component';
+import { RegistroComponent } from './home/registro/registro.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+    {path: 'login',
+  component:LoginComponent,},
+
+  {path:'',
+  component:CentralComponent,
+  pathMatch: 'full'},
+
+  {path: 'registro',
+  component:RegistroComponent,
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
