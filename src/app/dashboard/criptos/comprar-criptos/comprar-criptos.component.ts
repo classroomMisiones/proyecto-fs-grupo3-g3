09c34821044b2924dd5274e20e8ea1f15c4d9cc5
Fabian <lordf00@gmail.com>
2021-08-11 16:41:24 -0300

    Validación compra criptos, ajustes en navbar

--- src/app/dashboard/criptos/comprar-criptos/comprar-criptos.component.ts
+++ src/app/dashboard/criptos/comprar-criptos/comprar-criptos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormControl, Validators, FormGroup } from '@angular/forms'
+
+
 
 @Component({
   selector: 'app-comprar-criptos',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComprarCriptosComponent implements OnInit {
 
-  constructor() { }
+
+  createFormGroup() {
+    return new FormGroup ({
+      moneda: new FormControl('',[Validators.required]),
+      monto: new FormControl('',[Validators.required, Validators.min(1)]),
+      selectmoneda: new FormControl('',[Validators.required]),
+    });
+  }
+
+  formulario: FormGroup;
+
+  constructor() {
+    this.formulario = this.createFormGroup();
+  }
 
   ngOnInit(): void {
   }
+
+
+  verificar() {
+    if (this.formulario.valid) {
+        console.log('saved form');
+    }
+  }
+  get moneda() { return this.formulario.get('moneda'); } 
+  get monto() { return this.formulario.get('monto'); } 
+  get selectmoneda() { return this.formulario.get('selectmoneda'); } 
 
 }
