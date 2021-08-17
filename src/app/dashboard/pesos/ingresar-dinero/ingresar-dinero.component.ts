838c65f8afd8c7ac66a4f10554d538812e751841
Gustavo <gustavomisio@yahoo.com.ar>
2021-08-17 00:06:21 -0300

    correccion a formulario reactivo

--- src/app/dashboard/pesos/ingresar-dinero/ingresar-dinero.component.ts
+++ src/app/dashboard/pesos/ingresar-dinero/ingresar-dinero.component.ts
@@ -15,11 +15,22 @@ export class IngresarDineroComponent{
    this.buildForm();
    };
 
+   get usuarioNovalido(){
+    return this.form.get('usuario')?.invalid && this.form.get ('usuario')?.touched
+  }
+   get cvuNovalido(){
+   return this.form.get('cvu')?.invalid && this.form.get ('cvu')?.touched
+   }
+  get ingresarNovalido(){
+  return this.form.get('ingresar')?.invalid && this.form.get ('ingresar')?.touched
+   }
+
+
 
  private buildForm() {
  this.form = this.formBuilder.group({
- usuario: ['', [Validators.required]],
- cvu: ['', [Validators.required, Validators.maxLength(80)]],
+ usuario: ['', [Validators.required,Validators.pattern ('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+ cvu: ['', [Validators.required, Validators.maxLength(22)]],
  ingresar: ['', [Validators.required]],
   });
   
