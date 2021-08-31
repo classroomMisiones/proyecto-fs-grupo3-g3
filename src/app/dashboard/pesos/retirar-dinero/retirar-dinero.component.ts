7b9ffe1ab23862a3135d3e70849bc870fe09f6b8
Gustavo <gustavomisio@yahoo.com.ar>
2021-08-31 15:41:31 -0300

    arreglos retirar dinero

--- src/app/dashboard/pesos/retirar-dinero/retirar-dinero.component.ts
+++ src/app/dashboard/pesos/retirar-dinero/retirar-dinero.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-retirar-dinero',
   templateUrl: './retirar-dinero.component.html',
   styleUrls: ['./retirar-dinero.component.css']
 })
-export class RetirarDineroComponent implements OnInit {
+export class RetirarDineroComponent {
+   form!: FormGroup;
 
-  constructor() { }
+   constructor(private formBuilder: FormBuilder 
+     ){
+    this.buildForm();
+   };
 
-  ngOnInit(): void {
+//    get usuarioNovalido(){
+//     return this.form.get('usuario')?.invalid && this.form.get ('usuario')?.touched
+//   }
+//    get cvuNovalido(){
+//    return this.form.get('cvu')?.invalid && this.form.get ('cvu')?.touched
+//    }
+//   get retirarNovalido(){
+//   return this.form.get('retirar')?.invalid && this.form.get ('retirar')?.touched
+//    }
+
+
+
+  private buildForm() {
+  this.form = this.formBuilder.group({
+  usuario: ['', [Validators.required,Validators.pattern ('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+  cvu: ['', [Validators.required]],
+  retirar: ['', [Validators.required]],
+   });
+  
+   }
+   save(event: Event) {
+    event.preventDefault();
+     if(this.form.valid){
+       const value = this.form.value;
+      console.log(value);
+     }
   }
-
 }
