728c3957595b727cfdea6e789825c3b27fef7e4e
Gustavo <gustavomisio@yahoo.com.ar>
2021-10-20 19:06:40 -0300

    login

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -35,17 +35,43 @@ export class LoginComponent implements OnInit {
    private buildForm() {
     this.form = this.formBuilder.group({
       UserName:['', Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")], 
-      password:['',[Validators.required, Validators.minLength(8)]]
+      password:['',[Validators.required, Validators.minLength(6)]]
    
      });
     }
-    save(event: Event) {
-      event.preventDefault();
-      if(this.form.valid){
-        const value = this.form.value;
-        console.log(value);
+    // save(event: Event) {
+    //   event.preventDefault();
+    //   if(this.form.valid){
+    //     const value = this.form.value;
+    //     console.log(value);
+    //   }
+    // }
+    save(event: Event, usuario: LoginRequest)
+    {
+      event.preventDefault(); //Cancela la funcionalidad por default.
+      if (this.form.valid)
+      {
+        console.log(this.form.value); //se puede enviar al servidor...
+        this.authService.login(this.usuario)
+        .subscribe(
+          data => {
+          console.log("DATA"+ JSON.stringify( data));
+          //localStorage.setItem('auth-token', JSON.stringify(data ));
+  
+          this.router.navigate(['/dashboard']);
+         
+          },
+          error => {
+           this.error = error;
+          }
+        );
+      }
+      else
+      {
+        this.form.markAllAsTouched(); //Activa todas las validaciones
       }
     }
+    
 
 
 
